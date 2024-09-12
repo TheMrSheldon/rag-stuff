ed905810a84a78687efe3e6fd51a548c56c01c5f
Tim Hagen <timhagenmail@googlemail.com>
2024-09-12 13:17:09 +0000

    implemented reference view

--- src/components/referencebox.tsx
+++ src/components/referencebox.tsx
@@ -1,16 +1,46 @@
-import { Box } from '@mui/material';
+import { Avatar, Card, CardContent, CardHeader, Typography } from '@mui/material';
 import * as React from 'react';
 
 interface ReferenceBoxProps {
+    topic: any
+    refId: number
     reference: string
     unused: boolean
+    referenceData: any
 };
 
+const colors = ['#e6194b', '#3cb44b', '#ffe119', '#4363d8', '#f58231', '#911eb4', '#46f0f0', '#f032e6', '#bcf60c', '#fabebe', '#008080', '#e6beff', '#9a6324', '#fffac8', '#800000', '#aaffc3', '#808000', '#ffd8b1', '#000075', '#808080', '#ffffff', '#000000']
+
+
 const ReferenceBox: React.FC<ReferenceBoxProps> = (props: ReferenceBoxProps) => {
-    return (
-        <Box padding={"10pt"} width="200pt" sx={{opacity: props.unused? 0.5:1}}>
-            {props.reference}
-        </Box>
-    );
+    const doc = props.topic.candidates.find((obj: any) => obj.docid === props.reference)
+    if (!doc) {
+        return <Card sx={{ mb: 1, opacity: props.unused ? 0.5 : 1 }} variant="outlined">
+        <CardHeader
+            avatar={<Avatar aria-label="recipeid" sx={{backgroundColor: "red"}}>E</Avatar>}
+            title="Error"
+        />
+        <CardContent>
+            <Typography variant="body2" color="text.error">
+                Failed to fetch reference by ID {props.reference}
+            </Typography>
+        </CardContent>
+    </Card>
+    } else {
+        return (
+            <Card sx={{ mb: 1, opacity: props.unused ? 0.5 : 1, width: "500pt" }} variant="outlined">
+                <CardHeader
+                    avatar={<Avatar aria-label="recipeid" sx={{backgroundColor: colors[props.refId % colors.length]}}>{props.refId}</Avatar>}
+                    title={doc.doc.title}
+                    subheader={props.reference}
+                />
+                <CardContent>
+                    <Typography variant="body2" color="text.secondary">
+                        {doc.doc.segment}
+                    </Typography>
+                </CardContent>
+            </Card>
+        );
+    }
 }
 export default ReferenceBox;
--- src/features/analysisview/view.tsx
+++ src/features/analysisview/view.tsx
@@ -11,21 +11,23 @@ function fromJSONL(text: string): Snapshot[] {
 
 const PopulatedView: React.FC<{ data: Snapshot[] }> = (props) => {
     const startDate = new Date(props.data[0].timestamp);
-    const endDate = new Date(props.data[props.data.length-1].timestamp);
+    const endDate = new Date(props.data[props.data.length - 1].timestamp);
     const [timestamp, setTimestamp] = useState<number>(endDate.getTime() - startDate.getTime());
     const [playing, setPlaying] = useState<boolean>(false);
     const [playbackSpeed, setPlaybackSpeed] = useState<number>(1);
+    const [referenceData, setReferenceData] = useState<any | null>(null);
 
     const wallTime = new Date(props.data[0].timestamp).getTime() + timestamp
 
-    const curData = props.data.find(d => new Date(d.timestamp).getTime() >= wallTime) ?? props.data[props.data.length-1]
-    const citedIdx = curData.data.answer.flatMap(a => a.citations).reverse()
+    const curData = props.data.find(d => new Date(d.timestamp).getTime() >= wallTime) ?? props.data[props.data.length - 1]
+    const citedIdxSet = new Set(curData.data.answer.flatMap(a => a.citations).reverse());
+    const citedIdx = Array.from(citedIdxSet.values())
 
-    // const cited = curData.data.references
-    const uncited = curData.data.references
+    const topic = referenceData ? referenceData.find((obj: any) => obj.query.id === curData.data.topicid) : null
+
     return <Box display="flex" flexDirection="column" gap="10pt" maxHeight="97vh" flexWrap="nowrap">
         <Typography variant="h6">{curData.data.topic}</Typography>
-        <Box display="flex" flexDirection="row" gap="10pt" flexWrap="nowrap" flexGrow={1} sx={{overflowY: "auto"}}>
+        <Box display="flex" flexDirection="row" gap="10pt" flexWrap="nowrap" flexGrow={1} sx={{ overflowY: "auto" }}>
             <Box display="flex" flexDirection="column" gap="10pt" flexWrap="nowrap" flexGrow={1}>
                 <Paper sx={{ flexGrow: 1 }}>
                     <AnswerField answer={curData.data.answer} raw_text={curData.data.raw_text} />
@@ -40,10 +42,14 @@ const PopulatedView: React.FC<{ data: Snapshot[] }> = (props) => {
                 </Paper>
             </Box>
             <Stack spacing="5pt" direction="column" sx={{ overflowY: "auto" }} minWidth="fit-content">
-                <DragDropFileUpload onFileUpload={(file: File) => { }}></DragDropFileUpload>
-                {citedIdx.map((i) => <ListItem><Paper><ReferenceBox reference={curData.data.references[i]} unused={false} /></Paper></ListItem>)}
-                <Divider>Uncited</Divider>
-                {uncited.map((ref) => <ListItem><Paper><ReferenceBox reference={ref} unused /></Paper></ListItem>)}
+                {
+                    referenceData === null ? <DragDropFileUpload onFileUpload={(file: File) => {
+                        file.text().then(fromJSONL).then(setReferenceData)
+                    }}></DragDropFileUpload> :
+                        <>{citedIdx.map((i) => <ListItem><ReferenceBox topic={topic} refId={i} reference={curData.data.references[i]} referenceData={referenceData} unused={false} /></ListItem>)}
+                            <Divider>Uncited</Divider>
+                            {curData.data.references.map((ref, index) => citedIdxSet.has(index)? <></>:<ListItem><ReferenceBox topic={topic} refId={index} reference={ref} referenceData={referenceData} unused /></ListItem>)}</>
+                }
             </Stack>
         </Box>
     </Box>
